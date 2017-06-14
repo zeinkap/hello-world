b04276594f762f434d6eb886b08bc38dc00b78a5
Zein <zeinkap@gmail.com>
2017-06-14 12:19:39 -0400

    Add files via upload

---
+++ exam_grade.js
@@ -0,0 +1,13 @@
+var examGrade = 85;		//Input exam grade here
+
+if (examGrade >= 90) {
+	console.log('A');
+}  else if (examGrade < 90 && examGrade >= 80) {
+	console.log('B');
+}  else if (examGrade < 80 && examGrade >= 70) {
+	console.log('C');
+}  else if (examGrade < 70 && examGrade >= 60) {
+	console.log('D');
+}  else {
+	console.log('F');
+}
